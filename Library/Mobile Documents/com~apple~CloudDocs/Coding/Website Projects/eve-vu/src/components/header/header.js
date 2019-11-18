164971f9cd61b5850fad35cf27831aa0ff4e1860
yirano <yirano@protonmail.ch>
2019-11-18 10:50:47 -0500

    Animate menuIcon on hover & the menu itself

---
+++ Library/Mobile Documents/com~apple~CloudDocs/Coding/Website Projects/eve-vu/src/components/header/header.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./header.scss";
+
+export class header extends Component {
+  state = {
+    menuActive: true
+  };
+
+  handleClick = () => {
+    this.setState(prevState => {
+      return (prevState.menuActive = !prevState.menuActive);
+    });
+  };
+  render() {
+    return (
+      <header className="header" id="header">
+        <h1 className="headerTitle">eV</h1>
+        <button className="menuIcon" onClick={this.handleClick}>
+          <span />
+          <span />
+        </button>
+        <nav className={this.state.menuActive ? "show" : "hide"}>
+          <ul>
+            <li>
+              <a href="/">home</a>
+            </li>
+            <li>
+              <a href="/">portfolio</a>
+            </li>
+            <li>
+              <a href="/">about</a>
+            </li>
+            <li>
+              <a href="/">contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="hero" />
+      </header>
+    );
+  }
+}
+
+export default header;
